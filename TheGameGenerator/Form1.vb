9acf6dfb80df67b9d236817a327373dda1b0bb10
J2ghz <j2.00ghz@gmail.com>
2015-07-27 01:47:48 +0200

    Continue test configuration

--- TheGameGenerator/Form1.vb
+++ TheGameGenerator/Form1.vb
@@ -1,6 +1,5 @@
 ﻿Public Class Form1
-
+    Dim DB As New TheGameDB()
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
     End Sub
 End Class
--- TheGameGeneratorTests/CheckDB.vb
+++ TheGameGeneratorTests/CheckDB.vb
@@ -3,28 +3,27 @@ Imports Microsoft.VisualStudio.TestTools.UnitTesting
 
 <TestClass()> Public Class CheckDB
 
-    <TestMethod()> Public Sub ReadFirstVeci()
-        Throw New NotImplementedException
+    Private _testContext As TestContext
+    Public Property TestContext() As TestContext
+        Get
+            Return _testContext
+        End Get
+        Set(ByVal value As TestContext)
+            _testContext = value
+        End Set
+    End Property
+
+
+    <DataSource("veci")> <TestMethod()> Public Sub TestVeci()
+        Assert.IsNotNull(TestContext.DataRow)
     End Sub
 
-    <TestMethod()> Public Sub ReadFirstRecepty()
-        Throw New NotImplementedException
+    <DataSource("TheGameDB")> <TestMethod()> Public Sub TestRecepty()
+        Assert.IsNotNull(TestContext.DataRow)
     End Sub
 
-    <TestMethod()> Public Sub ReadFirstVyzkumy()
-        Throw New NotImplementedException
-    End Sub
-
-    <TestMethod()> Public Sub TestVeci()
-        Throw New NotImplementedException
-    End Sub
-
-    <TestMethod()> Public Sub TestRecepty()
-        Throw New NotImplementedException
-    End Sub
-
-    <TestMethod()> Public Sub TestVyzkumy()
-        Throw New NotImplementedException
+    <DataSource("TheGameDB")> <TestMethod()> Public Sub TestVyzkumy()
+        Assert.IsNotNull(TestContext.DataRow)
     End Sub
 
 End Class
