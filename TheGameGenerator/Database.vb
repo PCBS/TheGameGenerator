659377af3053728827080fe3bbd415fc5720ec22
J2ghz <j2.00ghz@gmail.com>
2015-07-27 03:54:39 +0200

    add database class

---
+++ TheGameGenerator/Database.vb
@@ -0,0 +1,9 @@
+﻿Public Class Database
+    Dim DB As New TheGameDB()
+    Sub New()
+
+    End Sub
+    Public Function GetItemInfo(id As Integer) As veci
+        Return DB.vecis.Find(id)
+    End Function
+End Class
