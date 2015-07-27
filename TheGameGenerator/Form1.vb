34fbac725a905d2c269184403f56514bcee82354
J2ghz <j2.00ghz@gmail.com>
2015-07-27 21:42:29 +0200

    Successfully implement logging

--- TheGameGenerator/Form1.vb
+++ TheGameGenerator/Form1.vb
@@ -1,10 +1,26 @@
-﻿Public Class Form1
+﻿Imports Serilog
+Imports System.Runtime.InteropServices
+Public Class Form1
     Dim Data As New Database
-    Dim log As New Serilog.LoggerConfiguration
-    Private Sub Button1_Click(sender As Object, e As EventArgs)
-
-    End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Win32.AllocConsole()
+        Me.BringToFront()
+        Log.Logger = (New LoggerConfiguration).WriteTo.LiterateConsole.MinimumLevel.Verbose.CreateLogger()
+        Log.Information("Form1_Load")
+
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+End Class
+''' <summary>
+''' Used to Show console for logging and debugging
+''' </summary>
+''' <remarks></remarks>
+Public Class Win32
+    <DllImport("kernel32.dll")>
+    Public Shared Function AllocConsole() As Boolean
+    End Function
 End Class
