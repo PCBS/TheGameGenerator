a13c36f0e098a8cbcd089393198070d48cf7d165
J2ghz <j2.00ghz@gmail.com>
2015-07-27 20:54:49 +0200

    add serilog

--- TheGameGenerator/Form1.vb
+++ TheGameGenerator/Form1.vb
@@ -1,8 +1,10 @@
 ﻿Public Class Form1
     Dim Data As New Database
-
+    Dim log As New Serilog.LoggerConfiguration
     Private Sub Button1_Click(sender As Object, e As EventArgs)
 
     End Sub
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    End Sub
 End Class
