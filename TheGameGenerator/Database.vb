9a2939caaea326b4471d3b67d26c7fb6002ed026
J2ghz <j2.00ghz@gmail.com>
2015-07-27 20:49:13 +0200

    Change app structure a bit

--- TheGameGenerator/Database.vb
+++ TheGameGenerator/Database.vb
@@ -1,17 +1,10 @@
 ﻿Public Class Database
     Dim DB As New TheGameDB()
-    Structure lists
-        'TODO list of cpu a tak dal
-    End Structure
     Sub New()
 
     End Sub
     Public Function GetItemInfo(id As Integer) As veci
         Return DB.vecis.Find(id)
-    End Function
-
-    Public Function BuildLists() As lists
-        'todo vygenerovat listy a pak je vratit, volat metody na jednotlive listy
     End Function
 
 End Class
--- TheGameGenerator/Form1.vb
+++ TheGameGenerator/Form1.vb
@@ -1,10 +1,7 @@
 ﻿Public Class Form1
     Dim Data As New Database
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        MsgBox(Data.GetItemInfo(5).nazev)
-    End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
 
     End Sub
 
